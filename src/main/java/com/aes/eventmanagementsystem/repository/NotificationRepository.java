eb5f4d2fcbc1f6b549f6625ef08f8407c3424cc3
ahmeterensak <ahmeterensak@outlook.com>
2024-02-18 01:32:05 +0300

    Event Management System Core Project

---
+++ src/main/java/com/aes/eventmanagementsystem/repository/NotificationRepository.java
@@ -0,0 +1,21 @@
+package com.aes.eventmanagementsystem.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.aes.eventmanagementsystem.model.Event;
+import com.aes.eventmanagementsystem.model.Notification;
+import com.aes.eventmanagementsystem.model.User;
+
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+
+    Notification findByNotificationContentAndUserAndEvent(String notificationContent, User user, Event event);
+
+    @Query("SELECT n FROM Notification n JOIN n.user u WHERE u.userId = :userId")
+    List<Notification> findNotificationsByUserId(@Param("userId") int userId);
+
+    @Query("SELECT n FROM Notification n JOIN n.event e WHERE e.eventId = :eventId")
+    List<Notification> findNotificationsByEventId(@Param("eventId") int eventId);
+}
---
+++ src/main/java/com/aes/eventmanagementsystem/service/INotificationService.java
@@ -0,0 +1,53 @@
+package com.aes.eventmanagementsystem.service;
+
+import java.util.List;
+
+import com.aes.eventmanagementsystem.dto.NotificationDto;
+import com.aes.eventmanagementsystem.model.Notification;
+
+public interface INotificationService {
+
+    /**
+     * Save Notification into DB with notificationDto entity
+     * 
+     * @param notificationDto
+     */
+    void createNotification(NotificationDto notificationDto);
+
+    /**
+     * Save Notification into DB with notification entity
+     * 
+     * @param notification
+     */
+    void createNotification(Notification notification);
+
+    /**
+     * Find All Notifications by user's userId who created the notification
+     * 
+     * @param userId
+     * @return Notification List
+     */
+    List<Notification> fetchNotificationsByUserId(int userId);
+
+    /**
+     * Find all notifications by event's eventId which is related to notification
+     * 
+     * @param eventId
+     * @return Notification List
+     */
+    List<Notification> fetchNotificationsByEventId(int eventId);
+
+    /**
+     * Get All Notifications
+     * 
+     * @return Notification List
+     */
+    List<Notification> fetchAll();
+
+    /**
+     * Delete notification by notificationId
+     * 
+     * @param notificationId
+     */
+    void deleteNotifications(int notificationId);
+}
