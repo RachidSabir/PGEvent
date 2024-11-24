5f34a9f375cb6c81ba677a227773d41e416d2c33
Rachid Sabir <rachidsabir326@gmail.com>
2024-11-25 00:07:09 +0100

    full date update

--- src/main/java/com/rs/eventmanagementsystem/controller/UserController.java
+++ src/main/java/com/rs/eventmanagementsystem/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.rs.eventmanagementsystem.dto.NotificationDto;
